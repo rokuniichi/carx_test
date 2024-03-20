faf646c6046de3fe24bcfdbba35b674f2b1e5679
rokuniichi <solovily@hotmail.com>
2024-03-20 11:55:45 +0700

    Prototype grid as a separate branch
    This reverts commit 4e27022dfe658d488a9a5325104d0b79818dfe1d.

---
+++ Assets/Scripts/Grid/GridCell.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class GridCell : MonoBehaviour
+{
+    [SerializeField] private GameObject selection;
+    public void SetSelected(bool state)
+    {
+        selection.SetActive(state);
+    }
+}
