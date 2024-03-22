9ea1101593fc4ec1cfc90b7c43dcaafebf14be18
rokuniichi <solovily@hotmail.com>
2024-03-23 00:08:30 +0700

    More codestyle

--- Assets/Scripts/Data/TowerData.cs
+++ Assets/Scripts/Data/TowerData.cs
@@ -4,13 +4,13 @@
 public class TowerData : ScriptableObject
 {
     [Header("References")]
-    [SerializeField] public GameObject BasePrefab;
-    [SerializeField] public GameObject WeaponPrefab;
+    [SerializeField] public GameObject     BasePrefab;
+    [SerializeField] public GameObject     WeaponPrefab;
     [SerializeField] public ProjectileData ProjectileData;
     [Header("Visuals")]
     [SerializeField] public Vector3 WeaponPlacementOffset;
     [Header("Stats")]
-    [SerializeField] public float Cooldown;
-    [SerializeField] public float Range;
+    [SerializeField] public float     Cooldown;
+    [SerializeField] public float     Range;
     [SerializeField] public LayerMask TargetLayers;
 }
